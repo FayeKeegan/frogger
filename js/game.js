09eb6eb0cac8364dd5ff062c2a3c2e79b8c7e332
Faye Keegan <faye.keegan@gmail.com>
2015-09-06 13:35:58 -0700

    draw water and highway

---
+++ js/game.js
@@ -0,0 +1,24 @@
+(function(){
+  var Frogger = window.Frogger = window.Frogger || {};
+
+  var Game = Frogger.Game = function(dim_y, dim_x) {
+    this.dim_y = dim_y;
+    this.dim_x = dim_x
+  };
+
+
+  Game.prototype.draw = function (ctx) {
+    var that = this;
+    ctx.fillStyle = "#63B8FF";
+    ctx.fillRect (0, 0, this.dim_x, this.dim_y / 2);
+
+    ctx.fillStyle = "#D6D6D6";
+    ctx.fillRect (0, this.dim_y / 2, this.dim_x, this.dim_y / 2);
+  };
+
+  Game.prototype.step = function(){
+    
+  };
+
+
+})();
