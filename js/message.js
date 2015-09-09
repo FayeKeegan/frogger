57a29c3d305301071f8dfee41f35b4d080be4a1c
Faye Keegan <faye.keegan@gmail.com>
2015-09-08 22:29:10 -0700

    message displays passed in mesage

--- js/message.js
+++ js/message.js
@@ -3,14 +3,14 @@
 
 	var Message = Frogger.Message = function(options){
 		this.game = options.game;
-		this.text = options.text;
+		this.message = options.message;
 	};
 
 	Message.prototype.draw = function(ctx){
 		ctx.font = "30px Comic Sans MS";
 		ctx.fillStyle = "red";
 		ctx.textAlign = "center";
-		ctx.fillText("Hello World", this.game.dim_x / 2, this.game.dim_y/2); 
+		ctx.fillText(this.message, this.game.dim_x / 2, this.game.dim_y/2); 
 	};
 
 })();
