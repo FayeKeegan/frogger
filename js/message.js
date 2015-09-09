99ca91df1dbb8efd8e28556a9f7810e7ddfbe010
Faye Keegan <faye.keegan@gmail.com>
2015-09-08 22:46:26 -0700

    change font on alerts, re-enable bootstrap

--- js/message.js
+++ js/message.js
@@ -8,7 +8,7 @@
 	};
 
 	Message.prototype.draw = function(ctx){
-		ctx.font = "30px Roboto";
+		ctx.font = "25px Helvetica Neue";
 		ctx.fillStyle = (this.alertType === "good" ? "#008B45" : "#FF6A6A");
 		ctx.textAlign = "center";
 		ctx.fillText(this.message, this.game.dim_x / 2, this.game.dim_y/2); 
