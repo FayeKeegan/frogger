1f59faa7591d4b7737cefa7f843263f91e943a0a
Faye Keegan <faye.keegan@gmail.com>
2015-09-10 14:39:03 -0700

    frog eyes moves when he changes directions

--- js/frog.js
+++ js/frog.js
@@ -8,9 +8,10 @@
     this.radius = Frog.RADIUS;
     this.pos = [this.game.dim_x / 2, this.game.dim_y - 15]
     this.color = Frog.COLOR;
+    this.previousDirection = [0, -1];
   };
 
-  Frog.COLOR = "rgb(78, 159, 21)";
+  Frog.COLOR = "pink";
   Frog.RADIUS = 10;
   Frog.VEL = [0,0];
 
@@ -19,6 +20,7 @@
   Frog.prototype.leap = function(direction){
     this.pos[0] += direction[0] * this.game.lane_y;
     this.pos[1] += direction[1] * this.game.lane_y;
+    this.previousDirection = direction;
   };
 
   Frog.prototype.draw = function(ctx){
@@ -41,12 +43,52 @@
     ctx.closePath();
   }
 
+  Frog.prototype.translateDirection = function(){
+    var x = this.previousDirection[0];
+    var y = this.previousDirection[1];
+    if (x === -1){
+      return "left";
+    } else if (x === 1){
+      return "right"
+    } else if (y === 1){
+      return "down"
+    } else if (y === -1){
+      return "up"
+    }
+  }
+
   Frog.prototype.drawEyes = function(ctx){
-    var leftX = this.pos[0] - this.radius / 3;
-    var rightX = this.pos[0] + this.radius / 3;
-    var eyeY =  this.pos[1] - this.radius / 2;
-    this.drawEye(ctx, [leftX, eyeY]);
-    this.drawEye(ctx, [rightX, eyeY]);
+    distToEye = this.radius / 2;
+    distBetweenEyes = this.radius / 3;
+    var direction = this.translateDirection();
+    var directionHash = {
+      "up": {"leftX" : this.pos[0] - distBetweenEyes,
+             "rightX": this.pos[0] + distBetweenEyes,
+              "leftY": this.pos[1] - distToEye,
+              "rightY":this.pos[1] - distToEye
+            },
+      "down": {"leftX" : this.pos[0] - distBetweenEyes,
+             "rightX": this.pos[0] + distBetweenEyes,
+              "leftY": this.pos[1] + distToEye,
+              "rightY":this.pos[1] + distToEye
+            },
+      "left": {"leftX" : this.pos[0] - distToEye,
+             "rightX": this.pos[0] - distToEye,
+              "leftY": this.pos[1] + distBetweenEyes,
+              "rightY":this.pos[1] - distBetweenEyes
+            },
+      "right": {"leftX" : this.pos[0] + distToEye,
+             "rightX": this.pos[0] + distToEye,
+              "leftY": this.pos[1] - distBetweenEyes,
+              "rightY":this.pos[1] + distBetweenEyes
+            }
+    }
+    var leftX = directionHash[direction]["leftX"]
+    var rightX = directionHash[direction]["rightX"]
+    var leftY =  directionHash[direction]["leftY"]
+    var rightY =  directionHash[direction]["rightY"]
+    this.drawEye(ctx, [leftX, leftY]);
+    this.drawEye(ctx, [rightX, rightY]);
   }
 
   Frog.prototype.drawEye = function(ctx, pos){
@@ -60,7 +102,7 @@
     var path = new Path2D();
     var endAngle = Math.PI;
     ctx.beginPath();
-    ctx.arc(pos[0],pos[1] - 1.5,1.5,Math.PI,-Math.PI,false);
+    ctx.arc(pos[0],pos[1],1.5,Math.PI,-Math.PI,false);
     ctx.lineTo(pos[0],pos[1]);
     ctx.fillStyle = "black"
     ctx.fill();
--- js/message.js
+++ js/message.js
@@ -8,8 +8,8 @@
 	};
 
 	Message.prototype.draw = function(ctx){
-		ctx.font = "25px Helvetica Neue";
-		ctx.fillStyle = (this.alertType === "good" ? "#008B45" : "#FF6A6A");
+		ctx.font = "25px sans-serif";
+		ctx.fillStyle = (this.alertType === "good" ? "white" : "#black");
 		ctx.textAlign = "center";
 		ctx.fillText(this.message, this.game.dim_x / 2, this.game.dim_y/2); 
 	};
